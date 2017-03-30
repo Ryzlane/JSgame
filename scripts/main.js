76c941fa9e66a884d6d339d094d1bc5336e1a341
Ryzlane <ryzlane@free.fr>
2017-03-30 21:30:32 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,6 @@
 
 var buttonStart = document.querySelector("#buttonStart");
 var rules = document.querySelector("#rules");
-var divLives = document.getElementById("lives");
-var divScore = document.getElementById("score");
 var doorAndCharacterL1 = document.getElementById("doorAndCharacterL1");
 var doorAndCharacterL2 = document.getElementById("doorAndCharacterL2");
 var doorAndCharacterL3 = document.getElementById("doorAndCharacterL3");
@@ -12,21 +10,6 @@ var doorAndCharacterR2 = document.getElementById("doorAndCharacterR2");
 var doorAndCharacterR3 = document.getElementById("doorAndCharacterR3");
 var doorsAndCharacters = [doorAndCharacterL1, doorAndCharacterL2, doorAndCharacterL3, doorAndCharacterR1, doorAndCharacterR2, doorAndCharacterR3];
 var answers = document.getElementsByClassName("avatars");
-var scoreTotal = document.getElementById("scoreTotal").innerText;
-
-//****************************** VAR AUDIO ****************************
-
-var doorOpening = new Audio();
-doorOpening.src = '../sounds/openDoor.wav'; // Opendoor
-
-var doorClosing = new Audio();
-doorClosing.src = '../sounds/closeDoor.wav'; // Close door
-
-var playButton = new Audio();
-playButton.src = '../sounds/play.wav'; // Game start
-
-var gameMusic = new Audio();
-gameMusic.src = '..sounds/game.wav'; // Game theme
 
 // **************************** FONCTIONS ****************************
 
@@ -74,30 +57,6 @@ function randomQuote() {
   var selectCharacter = Math.floor(Math.random() * characters.length);
   var selectSentence = Math.floor(Math.random() * characters[selectCharacter].sentences.length);
   return characters[selectCharacter].sentences[selectSentence];
-}
-
-function loseLife() {
-  var actualHealthBar = divLives.lastElementChild.src;
-  var actualHealth = actualHealthBar[actualHealthBar.length - 5];
-  var newHealth = actualHealth - 1;
-  if (newHealth >= 0) {
-    divLives.lastElementChild.src = '../images/lives/' + newHealth + '.png';
-  }
-}
-
-function gainScore() {
-  var actualScoreBar = divScore.lastElementChild.src;
-  var actualScore = actualScoreBar[actualScoreBar.length - 5];
-  var newScore = parseInt(actualScore) + 1;
-  if (newScore <= 4) {
-    divScore.lastElementChild.src = '../images/progress/' + newScore + '.png';
-  }
-}
-
-function gainScoreTotal(){
-  oldScore = scoreTotal.substr(14);
-  newScore = parseInt(oldScore) + 10;
-  scoreTotal += newScore;
 }
 
 // **************************** START THE GAME ****************************
