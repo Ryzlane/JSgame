3b1298e9649d9004b243e93d2e7a5cec3f1b84fa
Ryzlane <ryzlane@free.fr>
2017-03-31 03:14:15 +0200

    Add files via upload

---
+++ scripts/initialisation.js
@@ -0,0 +1,66 @@
+// **************************** START THE GAME ****************************
+
+var buttonStart = document.querySelector("#buttonStart");
+var rules = document.querySelector("#rules");
+
+// GENERATE A RANDOM QUOTE ON THE TOP OF THE GAME
+function randomQuote() {
+  var selectCharacter = Math.floor(Math.random() * characters.length);
+  var selectSentence = Math.floor(Math.random() * characters[selectCharacter].sentences.length);
+  return characters[selectCharacter].sentences[selectSentence];
+}
+
+document.getElementById("sentence").textContent = randomQuote();
+
+// REMOVE RULES
+buttonStart.addEventListener('click', function () {
+    rules.style.display = "none";
+    setInterval(moveADoor, 1000);
+  },
+  false
+);
+
+// CLICK ON CHARACTER
+for (l = 0; l < answers.length; l++) {
+  answers[l].addEventListener('click', function () {
+      if (this.classList.contains("falseAnswer") === true) {
+        console.log("PERDU !");
+        loseLife();
+      } else {
+        document.getElementById("sentence").textContent = randomQuote();
+        gainScore();
+        gainScoreTotal();
+      }
+    },
+    false
+  );
+
+}
+
+// **************************** RESTART - LOSE VER. ****************************
+
+
+function lost(){
+  clearInterval(setInterval(moveADoor, 1000));
+  rules.style.display = "block";
+  document.querySelector("#rulesText").style.display = "none";
+  document.querySelector("#winText").style.display = "none";
+  document.querySelector("#loseText").style.display = "block";
+  divLives.lastElementChild.src='../images/lives/3.png';
+  divScore.lastElementChild.src='../images/progress/0.png';
+  
+}
+
+
+// **************************** RESTART - WIN VER. ****************************
+
+function won(){
+  clearInterval(setInterval(moveADoor, 1000));
+  rules.style.display = "block";
+  document.querySelector("#rulesText").style.display = "none";
+  document.querySelector("#loseText").style.display = "none";
+  document.querySelector("#winText").style.display = "block";
+  divScore.lastElementChild.src='../images/progress/0.png';
+  divLives.lastElementChild.src='../images/lives/3.png';
+  
+}
